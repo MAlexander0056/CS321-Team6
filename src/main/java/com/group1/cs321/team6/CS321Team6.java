726f4d294cfc7add8a88124b8ea99eba082cc755
Malexander <maa0056@uah.edu>
2025-02-09 22:31:36 -0600

    First Commit

---
+++ src/main/java/com/group1/cs321/team6/CS321Team6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group1.cs321.team6;
+
+/**
+ *
+ * @author Admin
+ */
+public class CS321Team6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
