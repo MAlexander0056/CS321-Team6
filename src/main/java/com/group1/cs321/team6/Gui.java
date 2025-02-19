73fa2464f62b2f74de7b2b7e8b4f6705e17090d2
Michael A <maa0056@uah.edu>
2025-02-19 12:59:59 -0600

    Added basic skele for gui

---
+++ src/main/java/com/group1/cs321/team6/Gui.java
@@ -0,0 +1,28 @@
+package com.group1.cs321.team6;
+// CHANGE THIS - Specify which libray and include only what is needed
+import javax.swing.*;
+
+/**
+ * @author TBD
+ */
+
+
+ /**
+  * This class will organize all of the different pages that we will use to display our information.
+  *The goal is to have as many reusable components as possible when working with the GUI for code that is easy to maintain and update. 
+  */
+public class Gui {
+
+    /**
+     * Window for introduction page. Requests information needed for 
+     */
+    void CreateMainWindow(){
+
+    }
+    /**
+     * Window will display processed information
+     */
+    void CreateDisplayWindow(){
+
+    }
+}
